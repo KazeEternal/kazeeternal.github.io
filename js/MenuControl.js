033f9198693e2e77aeea51ff7adfd685b32389ba
Kyle Peplow <kyle.a.peplow@tutamail.com>
2023-10-29 23:52:01 -0400

    Broken Display Area setting from loaded html. HTML returns empty string.

---
+++ js/MenuControl.js
@@ -0,0 +1,18 @@
+function Display(target, source)
+{
+    var client = new XMLHttpRequest();
+    client.open('GET', '/html/' + source + '.html');
+    client.onreadystatechange = function() 
+    {
+        if(client.readyState == 3) 
+        {
+            alert(client.statusText);
+        }
+        else if(client.readyState = 4)
+        {
+            var targetArea = document.getElementById(target);
+            targetArea.innerHTML = client.responseText;
+        }
+    }
+    client.send();
+}
