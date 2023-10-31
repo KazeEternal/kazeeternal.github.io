325f5b494ac3db7dd1d950f8523cf6726a0b7347
Kyle Peplow <kyle.a.peplow@tutamail.com>
2023-10-30 22:11:43 -0400

    Navigation Menu highlight toggle and dynamic content loading for the display area completed.

--- js/MenuControl.js
+++ js/MenuControl.js
@@ -1,5 +1,10 @@
-function Display(target, source)
+function Display(target, source, callback)
 {
+    var args = Object.values(arguments); 
+    args.shift(); 
+    args.shift(); 
+    args.shift(); 
+
     var client = new XMLHttpRequest();
     client.open('GET', '/html/' + source + '.html');
     client.onreadystatechange = function() 
@@ -11,7 +16,22 @@ function Display(target, source)
         {
             var targetArea = document.getElementById(target);
             targetArea.innerHTML = client.responseText;
+            if(callback != null)
+            {
+                callback(...args);
+            }
         }
     }
     client.send();
+}
+
+function SwitchHeader(currentElement)
+{
+    var children = document.getElementById('navbar').childNodes;
+    children.forEach(function(item)
+    {
+        if(item.classList != null)
+        item.classList.remove('active');
+    });
+    currentElement.classList.add("active");
 }
